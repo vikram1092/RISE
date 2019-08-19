c393438b0bb2a5d01180abe055bb5754be7979be
Vikram Ramkumar <vik.r92@gmail.com>
2019-08-19 10:18:16 -0700

    Initial Commit

---
+++ RISE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RISE
+//
+//  Created by Vikram Ramkumar on 8/19/19.
+//  Copyright © 2019 Vikram Ramkumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RISETests/RISETests.swift
@@ -0,0 +1,34 @@
+//
+//  RISETests.swift
+//  RISETests
+//
+//  Created by Vikram Ramkumar on 8/19/19.
+//  Copyright © 2019 Vikram Ramkumar. All rights reserved.
+//
+
+import XCTest
+@testable import RISE
+
+class RISETests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
