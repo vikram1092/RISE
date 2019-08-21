36d56985d3be01a3a8eec7de7cd0df7a064c48ee
Vikram Ramkumar <vik.r92@gmail.com>
2019-08-21 10:33:11 -0700

    Adds assets and UI to show properties

--- RISE/ui/property list/PropertyListVC.swift
+++ RISE/ui/property list/PropertyListVC.swift
@@ -2,19 +2,31 @@ import UIKit
 import RxSwift
 
 enum StateState {
-    case Denver
-    case Houston
+    case Colorado
+    case Texas
 }
 
 class PropertyListVC: UIViewController {
     @IBOutlet weak var propertiesCollectionView: UICollectionView!
-    @IBOutlet weak var button1: UIButton!
-    
-    @IBOutlet weak var button2: UIButton!
-    @IBOutlet weak var button3: UIButton!
     @IBOutlet weak var coloradoButton: UIButton!
     @IBOutlet weak var texasButton: UIButton!
     @IBOutlet weak var propertiesLabel: UILabel!
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var property1Label: UILabel!
+    @IBOutlet weak var property2Label: UILabel!
+    @IBOutlet weak var property3Label: UILabel!
+    @IBOutlet weak var city1Label: UILabel!
+    @IBOutlet weak var city2Label: UILabel!
+    @IBOutlet weak var city3Label: UILabel!
+    @IBOutlet weak var units1Label: UILabel!
+    @IBOutlet weak var units2Label: UILabel!
+    @IBOutlet weak var units3Label: UILabel!
+    @IBOutlet weak var view1Button: UIButton!
+    @IBOutlet weak var view2Button: UIButton!
+    @IBOutlet weak var view3Button: UIButton!
     
     let disposables = CompositeDisposable()
     
@@ -25,7 +37,10 @@ class PropertyListVC: UIViewController {
     }
     
     func setupViews() {
-        changeToState(forState: .Denver)
+        changeToState(forState: .Colorado)
+        view1Button.layer.cornerRadius = 4
+        view2Button.layer.cornerRadius = 4
+        view3Button.layer.cornerRadius = 4
     }
     
     func setupButtons() {
@@ -42,12 +57,12 @@ class PropertyListVC: UIViewController {
         
         let denverState = coloradoButton.rx.tap
             .observeOn(MainScheduler.instance)
-            .map { self.changeToState(forState: .Denver) }
+            .map { self.changeToState(forState: .Colorado) }
             .subscribe()
         
         let houstonState = texasButton.rx.tap
             .observeOn(MainScheduler.instance)
-            .map { self.changeToState(forState: .Houston) }
+            .map { self.changeToState(forState: .Texas) }
             .subscribe()
         
         _ = disposables.insert(propertyButtons)
@@ -57,12 +72,12 @@ class PropertyListVC: UIViewController {
     
     func changeToState(forState state: StateState) {
         switch state {
-        case .Denver:
-            propertiesLabel.text = "Denver Properties"
-            showPropertiesFor(.Denver)
-        case .Houston:
-            propertiesLabel.text = "Houston Properties"
-            showPropertiesFor(.Houston)
+        case .Colorado:
+            propertiesLabel.text = "Colorado Properties"
+            showPropertiesFor(.Colorado)
+        case .Texas:
+            propertiesLabel.text = "Texas Properties"
+            showPropertiesFor(.Texas)
         }
     }
     
